50e0d33f78844e2dd691f7eadca44705cb408190
Muhamad-rifki <72338988+Muhamad-rifki@users.noreply.github.com>
2022-01-15 00:35:14 +0700

    Add files via upload

---
+++ phpcrud/phpcrud/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Dashboard</h2>
+	<p>Selamat Datang di Aplikasi Penambahan Kontak</p>
+</div>
+
+<?=template_footer()?>
